b6368fd7bd972368ce0f670c51b9731e7306123b
Shiva Kumar Talamanchi <shivakumar@Shivas-MacBook-Air.local>
2024-10-29 19:43:41 -0400

    Initial backend setup with Spring Boot

---
+++ itune/src/main/java/youtube/com/itune/ItuneApplication.java
@@ -0,0 +1,13 @@
+package youtube.com.itune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItuneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItuneApplication.class, args);
+	}
+
+}
---
+++ itune/src/test/java/youtube/com/itune/ItuneApplicationTests.java
@@ -0,0 +1,13 @@
+package youtube.com.itune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
